f2de33743ff7623cc301c64a39f3ae104e6c4253
GraceeFlower <1491644103@qq.com>
2020-02-17 15:38:48 +0800

    [WM] feat: get the menu about all the dishes

--- src/main/java/com/thoughtworks/App.java
+++ src/main/java/com/thoughtworks/App.java
@@ -1,8 +1,15 @@
 package com.thoughtworks;
 
+import java.util.Scanner;
+
 public class App {
 
   public static void main(String[] args) {
-
+    Scanner getOrder = new Scanner(System.in);
+    System.out.println("请点菜：");
+    String selectedItems = getOrder.nextLine();
+    Restaurant restaurant = new Restaurant();
+    System.out.println("这是您的订单详情：");
+    System.out.println(restaurant.bestCharge(selectedItems));
   }
 }
--- src/main/java/com/thoughtworks/Restaurant.java
+++ src/main/java/com/thoughtworks/Restaurant.java
@@ -1,8 +1,32 @@
 package com.thoughtworks;
 
+import java.util.ArrayList;
+
 public class Restaurant {
 
   public String bestCharge(String selectedItems) {
-    return null;
+    ArrayList<String> menu = printMenu(selectedItems);
+    StringBuilder test = new StringBuilder();
+    for (String i: menu) {
+      test.append(i);
+    }
+    return test.toString();
   }
+
+  public ArrayList<String> printMenu(String selectedItems) {
+    ArrayList<String> menu = new ArrayList<>();
+    String[] items = selectedItems.split(",");
+    for (String i: items) {
+      String[] item = i.split(" x ");
+      for (Dish j: DataProvider.getDishes()) {
+        if (j.getId().equals(item[0])) {
+          int subtotal = (int) (j.getPrice() * Integer.parseInt(item[1]));
+          String dish = j.getName() + " x " + item[1] + " = " + subtotal;
+          menu.add(dish);
+        }
+      }
+    }
+    return menu;
+  }
+
 }
