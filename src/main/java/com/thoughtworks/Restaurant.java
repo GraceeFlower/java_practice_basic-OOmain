2fd903dc37dd03f47d976197468fb1d2eb7a4fdc
GraceeFlower <1491644103@qq.com>
2020-02-17 18:04:52 +0800

    [WM] refactor: add a menu parser to handle the input menu

--- src/main/java/com/thoughtworks/Restaurant.java
+++ src/main/java/com/thoughtworks/Restaurant.java
@@ -5,25 +5,11 @@ import java.util.ArrayList;
 public class Restaurant {
 
   public String bestCharge(String selectedItems) {
-    ArrayList<Dish> menu = getMenu(selectedItems);
+    MenuParser parser = new MenuParser(selectedItems);
+    ArrayList<Dish> menu = parser.getMenuList();
     int[] subtotal = getSubtotal(menu);
     String strategy = getStrategy(menu, subtotal);
     return printReceipt(menu, subtotal, strategy);
-  }
-
-  public ArrayList<Dish> getMenu(String selectedItems) {
-    ArrayList<Dish> menu = new ArrayList<>();
-    String[] items = selectedItems.split(",");
-    for (String i: items) {
-      String[] item = i.split(" x ");
-      for (Dish j: DataProvider.getDishes()) {
-        if (j.getId().equals(item[0])) {
-          menu.add(j);
-          menu.get(menu.size() - 1).setCount(item[1]);
-        }
-      }
-    }
-    return menu;
   }
 
   public int[] getSubtotal(ArrayList<Dish> menu) {
