0f91eaef1c4532dfc6a2b75b1ad6cfab82ff8ca3
GraceeFlower <1491644103@qq.com>
2020-02-17 16:07:02 +0800

    [WM] refactor: use ArrayList to save the menu

--- src/main/java/com/thoughtworks/Dish.java
+++ src/main/java/com/thoughtworks/Dish.java
@@ -5,11 +5,20 @@ public class Dish {
   private String id;
   private String name;
   private double price;
+  private String count;
 
   public Dish(String id, String name, double price) {
     this.id = id;
     this.name = name;
     this.price = price;
+  }
+
+  public int getCount() {
+    return Integer.parseInt(count);
+  }
+
+  public void setCount(String count) {
+    this.count = count;
   }
 
   public String getId() {
--- src/main/java/com/thoughtworks/Restaurant.java
+++ src/main/java/com/thoughtworks/Restaurant.java
@@ -5,32 +5,34 @@ import java.util.ArrayList;
 public class Restaurant {
 
   public String bestCharge(String selectedItems) {
-    ArrayList<String> menu = getMenu(selectedItems);
+    ArrayList<Dish> menu = getMenu(selectedItems);
     return printReceipt(menu);
   }
 
-  public ArrayList<String> getMenu(String selectedItems) {
-    ArrayList<String> menu = new ArrayList<>();
+  public ArrayList<Dish> getMenu(String selectedItems) {
+    ArrayList<Dish> menu = new ArrayList<>();
     String[] items = selectedItems.split(",");
     for (String i: items) {
       String[] item = i.split(" x ");
       for (Dish j: DataProvider.getDishes()) {
         if (j.getId().equals(item[0])) {
-          int subtotal = (int) (j.getPrice() * Integer.parseInt(item[1]));
-          String dish = j.getName() + " x " + item[1] + " = " + subtotal + "元\n";
-          menu.add(dish);
+          menu.add(j);
+          menu.get(menu.size() - 1).setCount(item[1]);
         }
       }
     }
     return menu;
   }
 
-  public String printReceipt(ArrayList<String> menu) {
-    StringBuilder test = new StringBuilder();
-    for (String i: menu) {
-      test.append(i);
+
+
+  public String printReceipt(ArrayList<Dish> menu) {
+    StringBuilder Receipt = new StringBuilder("============= 订餐明细 =============\n");
+    for (Dish dish: menu) {
+      Receipt.append(dish.getName()).append(" x ").append(dish.getCount())
+        .append(" = ").append((int) dish.getPrice() * dish.getCount()).append("元\n");
     }
-    return test.toString();
+    return Receipt.toString();
   }
 
 }
