1129a066ae1c673f4cf1bd1db01e9258e4347edb
GraceeFlower <1491644103@qq.com>
2020-02-17 15:44:21 +0800

    [WM] refactor: standardized the menu & sort the printReceipt function out

--- src/main/java/com/thoughtworks/Restaurant.java
+++ src/main/java/com/thoughtworks/Restaurant.java
@@ -5,15 +5,11 @@ import java.util.ArrayList;
 public class Restaurant {
 
   public String bestCharge(String selectedItems) {
-    ArrayList<String> menu = printMenu(selectedItems);
-    StringBuilder test = new StringBuilder();
-    for (String i: menu) {
-      test.append(i);
-    }
-    return test.toString();
+    ArrayList<String> menu = getMenu(selectedItems);
+    return printReceipt(menu);
   }
 
-  public ArrayList<String> printMenu(String selectedItems) {
+  public ArrayList<String> getMenu(String selectedItems) {
     ArrayList<String> menu = new ArrayList<>();
     String[] items = selectedItems.split(",");
     for (String i: items) {
@@ -21,12 +17,20 @@ public class Restaurant {
       for (Dish j: DataProvider.getDishes()) {
         if (j.getId().equals(item[0])) {
           int subtotal = (int) (j.getPrice() * Integer.parseInt(item[1]));
-          String dish = j.getName() + " x " + item[1] + " = " + subtotal;
+          String dish = j.getName() + " x " + item[1] + " = " + subtotal + "元\n";
           menu.add(dish);
         }
       }
     }
     return menu;
+  }
+
+  public String printReceipt(ArrayList<String> menu) {
+    StringBuilder test = new StringBuilder();
+    for (String i: menu) {
+      test.append(i);
+    }
+    return test.toString();
   }
 
 }
