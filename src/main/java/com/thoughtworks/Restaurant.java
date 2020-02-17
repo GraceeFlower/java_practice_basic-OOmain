e34c7b5e27ed3a2356b7fcdf332d96501cffd656
GraceeFlower <1491644103@qq.com>
2020-02-17 16:46:01 +0800

    [WM] feat: add the half-off discount strategy

--- src/main/java/com/thoughtworks/Restaurant.java
+++ src/main/java/com/thoughtworks/Restaurant.java
@@ -7,7 +7,8 @@ public class Restaurant {
   public String bestCharge(String selectedItems) {
     ArrayList<Dish> menu = getMenu(selectedItems);
     int[] subtotal = getSubtotal(menu);
-    return printReceipt(menu, subtotal);
+    String strategy = getStrategy(menu);
+    return printReceipt(menu, subtotal, strategy);
   }
 
   public ArrayList<Dish> getMenu(String selectedItems) {
@@ -33,14 +34,46 @@ public class Restaurant {
     return subtotal;
   }
 
-  public String printReceipt(ArrayList<Dish> menu, int[] subtotal) {
-    StringBuilder Receipt = new StringBuilder("============= 订餐明细 =============\n");
+  public String getStrategy(ArrayList<Dish> menu) {
+    ArrayList<Integer> halfOffInfo = calculateHalfOff(menu);
+    StringBuilder strategy = new StringBuilder("-----------------------------------\n"
+      + "使用优惠:\n"
+      + "指定菜品半价(");
+    int infoLen = halfOffInfo.size();
+    for (int i = 0; i < infoLen - 1; i++) {
+      strategy.append(menu.get(i).getName());
+      if (i != infoLen - 2) {
+        strategy.append("，");
+      }
+    }
+    strategy.append(")，省").append(halfOffInfo.get(infoLen - 1)).append("元\n");
+    return strategy.toString();
+  }
+
+  public ArrayList<Integer> calculateHalfOff(ArrayList<Dish> menu) {
+    ArrayList<Integer> halfOffInfo = new ArrayList<>();
+    int reducePrice = 0;
     for (int i = 0; i < menu.size(); i++) {
       Dish dish = menu.get(i);
-      Receipt.append(dish.getName()).append(" x ").append(dish.getCount())
+      if(DataProvider.getHalfDishIds().contains(dish.getId())) {
+        reducePrice += (int)dish.getPrice() / 2;
+        halfOffInfo.add(i);
+      }
+    }
+    halfOffInfo.add(reducePrice);
+    return halfOffInfo;
+  }
+
+  public String printReceipt(ArrayList<Dish> menu, int[] subtotal, String strategy) {
+    StringBuilder receipt = new StringBuilder("============= 订餐明细 =============\n");
+    for (int i = 0; i < menu.size(); i++) {
+      Dish dish = menu.get(i);
+      receipt.append(dish.getName()).append(" x ").append(dish.getCount())
         .append(" = ").append(subtotal[i]).append("元\n");
     }
-    return Receipt.toString();
+    receipt.append(strategy);
+    receipt.append("===================================");
+    return receipt.toString();
   }
 
 }
